43c2afa82fe42ba4643d58331b18aa12b7bccbb6
jkh <jiangkh5521@gmail.com>
2026-03-28 11:23:31 +0000

    mirror codex session state for resume migration (sanitized)

---
+++ codex_sessions/shell_snapshots/019cb815-5ecb-7ce2-92e1-38c7a64d9578.1774692752261746689.sh
@@ -0,0 +1,58 @@
+# Snapshot file
+# Unset all aliases to avoid conflicts with functions
+# Functions
+
+# setopts 3
+set -o braceexpand
+set -o hashall
+set -o interactive-comments
+
+# aliases 0
+
+# exports 46
+declare -x BROWSER="/root/.cursor-server/bin/b29eb4ee5f9f6d1cb2afbc09070198d3ea6ad760/bin/helpers/browser.sh"
+declare -x CODEX_MANAGED_BY_NPM="1"
+declare -x COLORTERM="truecolor"
+declare -x CONDA_DEFAULT_ENV="base"
+declare -x CONDA_EXE="/mnt/workspace/zyq/miniconda3/bin/conda"
+declare -x CONDA_PREFIX="/mnt/workspace/zyq/miniconda3"
+declare -x CONDA_PROMPT_MODIFIER="(base) "
+declare -x CONDA_PYTHON_EXE="/mnt/workspace/zyq/miniconda3/bin/python"
+declare -x CONDA_SHLVL="1"
+declare -x GIT_ASKPASS="/root/.cursor-server/bin/b29eb4ee5f9f6d1cb2afbc09070198d3ea6ad760/extensions/git/dist/askpass.sh"
+declare -x HOME="/root"
+declare -x JAVA_HOME="/mnt/workspace/lkm/shared/miniconda3"
+declare -x JVM_PATH="/mnt/workspace/lkm/shared/miniconda3/lib/jvm/lib/server/libjvm.so"
+declare -x LANG="C.UTF-8"
+declare -x LESSCLOSE="/usr/bin/lesspipe %s %s"
+declare -x LESSOPEN="| /usr/bin/lesspipe %s"
+declare -x LOGNAME="root"
+declare -x LS_COLORS="rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:"
+declare -x MOTD_SHOWN="pam"
+declare -x OVSX_REGISTRY_URL="https://open-vsx.org"
+declare -x PATH="/root/.codex/tmp/arg0/codex-arg0hi8fty:/usr/local/node-v24.14.0-linux-x64/lib/node_modules/@openai/codex/node_modules/@openai/codex-linux-x64/vendor/x86_64-unknown-linux-musl/path:/usr/local/node-v24.14.0-linux-x64/bin:/root/.local/bin:/mnt/workspace/zyq/miniconda3/bin:/mnt/workspace/zyq/miniconda3/condabin:/root/.cursor-server/bin/b29eb4ee5f9f6d1cb2afbc09070198d3ea6ad760/bin/remote-cli:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+declare -x SHELL="/bin/bash"
+declare -x SHLVL="2"
+declare -x SSH_CLIENT="112.224.223.1 57686 22"
+declare -x SSH_CONNECTION="112.224.223.1 57686 192.168.0.159 22"
+declare -x TERM="xterm-256color"
+declare -x TERM_PROGRAM="vscode"
+declare -x TERM_PROGRAM_VERSION="2.6.20"
+declare -x TMPDIR="/mnt/workspace/tmp"
+declare -x USER="root"
+declare -x UV_CACHE_DIR="/mnt/workspace/tmp"
+declare -x UV_INDEX_URL="https://mirrors.aliyun.com/pypi/simple"
+declare -x UV_PYTHON_INSTALL_MIRROR="https://gh-proxy.com/https://github.com/astral-sh/python-build-standalone/releases/download"
+declare -x VSCODE_GIT_ASKPASS_EXTRA_ARGS=""
+declare -x VSCODE_GIT_ASKPASS_MAIN="/root/.cursor-server/bin/b29eb4ee5f9f6d1cb2afbc09070198d3ea6ad760/extensions/git/dist/askpass-main.js"
+declare -x VSCODE_GIT_ASKPASS_NODE="/root/.cursor-server/bin/b29eb4ee5f9f6d1cb2afbc09070198d3ea6ad760/node"
+declare -x VSCODE_GIT_IPC_AUTH_TOKEN="e98ee004be481330e9e2c3b7e11d9467d75de543a9ff81e067143fcfe1c0e9bf"
+declare -x VSCODE_GIT_IPC_HANDLE="/tmp/vscode-git-be2de7ce79.sock"
+declare -x VSCODE_IPC_HOOK_CLI="/tmp/vscode-ipc-87169f43-6886-4859-bae1-d0ae5a056441.sock"
+declare -x _CE_CONDA=""
+declare -x _CE_M=""
+declare -x _CONDA_EXE="/mnt/workspace/zyq/miniconda3/bin/conda"
+declare -x _CONDA_ROOT="/mnt/workspace/zyq/miniconda3"
+declare -x all_proxy="http://127.0.0.1:7897"
+declare -x http_proxy="http://127.0.0.1:7897"
+declare -x https_proxy="http://127.0.0.1:7897"
---
+++ codex_sessions/shell_snapshots/019d3361-b694-7532-ad19-eaec0168987b.sh
@@ -0,0 +1,61 @@
+# Snapshot file
+# Unset all aliases to avoid conflicts with functions
+# Functions
+
+# setopts 3
+set -o braceexpand
+set -o hashall
+set -o interactive-comments
+
+# aliases 0
+
+# exports 49
+declare -x BROWSER="/root/.vscode-server/bin/072586267e68ece9a47aa43f8c108e0dcbf44622/bin/helpers/browser.sh"
+declare -x BUNDLED_DEBUGPY_PATH="/root/.vscode-server/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/libs/debugpy"
+declare -x COLORTERM="truecolor"
+declare -x CONDA_DEFAULT_ENV="verl"
+declare -x CONDA_EXE="/mnt/workspace/zyq/miniconda3/bin/conda"
+declare -x CONDA_PREFIX="/mnt/workspace/luofuwen/envs/verl"
+declare -x CONDA_PREFIX_1="/mnt/workspace/zyq/miniconda3"
+declare -x CONDA_PROMPT_MODIFIER="(verl) "
+declare -x CONDA_PYTHON_EXE="/mnt/workspace/zyq/miniconda3/bin/python"
+declare -x CONDA_SHLVL="2"
+declare -x GIT_ASKPASS="/root/.vscode-server/bin/072586267e68ece9a47aa43f8c108e0dcbf44622/extensions/git/dist/askpass.sh"
+declare -x HOME="/root"
+declare -x JAVA_HOME="/mnt/workspace/lkm/shared/miniconda3"
+declare -x JVM_PATH="/mnt/workspace/lkm/shared/miniconda3/lib/jvm/lib/server/libjvm.so"
+declare -x LANG="C.UTF-8"
+declare -x LESSCLOSE="/usr/bin/lesspipe %s %s"
+declare -x LESSOPEN="| /usr/bin/lesspipe %s"
+declare -x LOGNAME="root"
+declare -x LS_COLORS="rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:"
+declare -x MOTD_SHOWN="pam"
+declare -x PATH="/root/.codex/tmp/arg0/codex-arg0NGFh2c:/usr/local/node-v24.14.0-linux-x64/bin:/root/.local/bin:/root/.vscode-server/bin/072586267e68ece9a47aa43f8c108e0dcbf44622/bin/remote-cli:/usr/local/node-v24.14.0-linux-x64/bin:/root/.local/bin:/mnt/workspace/luofuwen/envs/verl/bin:/mnt/workspace/zyq/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/root/.vscode-server/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/scripts/noConfigScripts"
+declare -x PYDEVD_DISABLE_FILE_VALIDATION="1"
+declare -x PYTHONSTARTUP="/root/.vscode-server/data/User/workspaceStorage/7cabf6019a9b4556a5ca4489b2d12c74/ms-python.python/pythonrc.py"
+declare -x PYTHON_BASIC_REPL="1"
+declare -x SHELL="/bin/bash"
+declare -x SHLVL="2"
+declare -x SSH_CLIENT="58.37.221.72 64445 22"
+declare -x SSH_CONNECTION="58.37.221.72 64445 192.168.0.159 22"
+declare -x TERM="xterm-256color"
+declare -x TERM_PROGRAM="vscode"
+declare -x TERM_PROGRAM_VERSION="1.109.5"
+declare -x TMPDIR="/mnt/workspace/tmp"
+declare -x USER="root"
+declare -x UV_CACHE_DIR="/mnt/workspace/tmp"
+declare -x UV_INDEX_URL="https://mirrors.aliyun.com/pypi/simple"
+declare -x UV_PYTHON_INSTALL_MIRROR="https://gh-proxy.com/https://github.com/astral-sh/python-build-standalone/releases/download"
+declare -x VSCODE_DEBUGPY_ADAPTER_ENDPOINTS="/root/.vscode-server/extensions/ms-python.debugpy-2025.18.0-linux-x64/.noConfigDebugAdapterEndpoints/endpoint-de53751b63a88846.txt"
+declare -x VSCODE_GIT_ASKPASS_EXTRA_ARGS=""
+declare -x VSCODE_GIT_ASKPASS_MAIN="/root/.vscode-server/bin/072586267e68ece9a47aa43f8c108e0dcbf44622/extensions/git/dist/askpass-main.js"
+declare -x VSCODE_GIT_ASKPASS_NODE="/root/.vscode-server/bin/072586267e68ece9a47aa43f8c108e0dcbf44622/node"
+declare -x VSCODE_GIT_IPC_HANDLE="/mnt/workspace/tmp/vscode-git-a54ce0a3ab.sock"
+declare -x VSCODE_IPC_HOOK_CLI="/tmp/vscode-ipc-b01004bb-a536-478b-9f0b-c34f945ecc75.sock"
+declare -x VSCODE_PYTHON_AUTOACTIVATE_GUARD="1"
+declare -x _CE_CONDA=""
+declare -x _CE_M=""
+declare -x _CONDA_EXE="/mnt/workspace/zyq/miniconda3/bin/conda"
+declare -x _CONDA_ROOT="/mnt/workspace/zyq/miniconda3"
+declare -x http_proxy="http://127.0.0.1:6789"
+declare -x https_proxy="http://127.0.0.1:6789"
